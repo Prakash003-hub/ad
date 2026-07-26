87ea481118adaca58766366f9841050d6791f4f8
Prakash003-hub <Prakash.131003k@gmail.com>
2026-07-26 20:27:29 +0530

    jekndcervcsceceuefewegrgtrhhbfgnfjvjhnfihui

--- src/components/SuccessPage.jsx
+++ src/components/SuccessPage.jsx
@@ -16,7 +16,7 @@ export default function SuccessPage() {
 
 👉 *இப்போதே பதிவு செய்யுங்கள்:*`
 
-  const shareUrl = typeof window !== 'undefined' ? window.location.origin : ''
+  const shareUrl = 'https://shortlinkjd.vercel.app/s/micronotes'
   const fullShareText = encodeURIComponent(`${shareMessage}\n${shareUrl}`)
 
   return (
